1bc60a4d3174408b2a70e87931df02dba7d89b64
davyvob <davy.van.overberghe@student.howest.be>
2022-01-14 09:50:06 +0100

    commit all

---
+++ Fortnite.GameEngine/FortniteTeam.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fortnite.GameEngine
+{
+    public class FortniteTeam
+    {
+        public const int MaximumPlayersInTeam = 4;
+        public static readonly string MaximumPlayersInTeamMessage = $"There cannot be more than {MaximumPlayersInTeam} players in the team";
+        public const string CannotDeletePlayerBecauseNoPlayersInTeamMessage = "Cannot remove player because there are no players in the team";
+
+        public List<FortnitePlayer> PlayersInTeam { get; set; }
+
+        public FortniteTeam()
+        {
+            PlayersInTeam = new List<FortnitePlayer>();
+        }
+
+        public void AddPlayerToTeam(FortnitePlayer player)
+        {
+            if (PlayersInTeam.Count <= 3)
+            {
+                PlayersInTeam.Add(player);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(null, MaximumPlayersInTeamMessage);
+            }
+        }
+
+        public void RemovePlayerFromTeam(FortnitePlayer player)
+        {
+            if (PlayersInTeam.Count < 1)
+            {
+                throw new Exception(CannotDeletePlayerBecauseNoPlayersInTeamMessage);
+            }
+            if (PlayersInTeam.Contains(player))
+            {
+                PlayersInTeam.Remove(player);
+            }
+        }
+    }
+}
---
+++ Fortnite.Tests/FortniteTeamTests.cs
@@ -0,0 +1,78 @@
+﻿using Fortnite.GameEngine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Fortnite.Tests
+{
+    public class FortniteTeamTests
+    {
+        
+        //Tests allready writen in startcode
+        [Fact]
+        public void AddPlayerToTeam_WithValidPlayer_UpdatesPlayersInTeam()
+        {
+            // Arrange
+            int expected = 1;
+            FortnitePlayer fortnitePlayer = new FortnitePlayer("Test Player", 100, 1);
+            FortniteTeam fortniteTeam = new FortniteTeam();
+
+            // Act
+            fortniteTeam.AddPlayerToTeam(fortnitePlayer);
+
+            // Assert
+            int actual = fortniteTeam.PlayersInTeam.Count;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void AddPlayerToTeam_ExceedsMaximumNumberOfPlayers_ThrowsArgumentOutOfRangeException()
+        {
+            // Arrange
+            FortniteTeam fortniteTeam = new FortniteTeam();
+
+            for (int i = 0; i < FortniteTeam.MaximumPlayersInTeam; i++)
+            {
+                fortniteTeam.AddPlayerToTeam(new FortnitePlayer($"TestPlayer_{i}", 100, 1));
+            }
+
+            // Assert on Act
+            ArgumentOutOfRangeException ex = Assert.Throws<ArgumentOutOfRangeException>(() => fortniteTeam.AddPlayerToTeam(new FortnitePlayer("NewTestPlayer", 100, 1)));
+            Assert.Equal(FortniteTeam.MaximumPlayersInTeamMessage, ex.Message);
+        }
+
+        [Fact]
+        public void RemovePlayerFromTeam_WithValidPlayer_UpdatesPlayersInTeam()
+        {
+            // Arrange
+            int expected = 3;
+            FortniteTeam fortniteTeam = new FortniteTeam();
+
+            for (int i = 0; i < FortniteTeam.MaximumPlayersInTeam; i++)
+            {
+                fortniteTeam.AddPlayerToTeam(new FortnitePlayer($"TestPlayer_{i}", 100, 1));
+            }
+
+            FortnitePlayer playerToRemove = fortniteTeam.PlayersInTeam[0];
+
+            // Act
+            fortniteTeam.RemovePlayerFromTeam(playerToRemove);
+
+            // Assert
+            int actual = fortniteTeam.PlayersInTeam.Count;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void RemovePlayerFromTeam_WhenThereAreNoPlayersInTeam_ThrowsException()
+        {
+            // Arrange
+            FortniteTeam fortniteTeam = new FortniteTeam();
+
+            // Assert on Act
+            Exception ex = Assert.Throws<Exception>(() => fortniteTeam.RemovePlayerFromTeam(new FortnitePlayer("TestPlayerToDelete", 100, 1)));
+            Assert.Equal(FortniteTeam.CannotDeletePlayerBecauseNoPlayersInTeamMessage, ex.Message);
+        }
+    }
+}
