c6bb01bad84de988f7f06552458235b37174c24d
davyvob <davy.van.overberghe@student.howest.be>
2022-01-14 09:53:01 +0100

    test

--- Fortnite.Wpf/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ Fortnite.Wpf/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -133,7 +133,7 @@ namespace Fortnite.Wpf {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Fortnite.Wpf;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Fortnite.Wpf;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
